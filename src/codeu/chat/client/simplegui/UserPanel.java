53f567da04f2824514f9f5ba672100162f5211cd
Sophey <sophey.dong@gmail.com>
2017-04-26 15:48:09 -0400

    Added gitignore file, got signin to vaguely work
    User is able to sign in after pressing “add”, but no more than one user
    is able to sign in.

--- src/codeu/chat/client/simplegui/UserPanel.java
+++ src/codeu/chat/client/simplegui/UserPanel.java
@@ -160,11 +160,9 @@ public final class UserPanel extends JPanel {
     userAddButton.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        final String s = (String) JOptionPane.showInputDialog(
-            UserPanel.this, "Enter user name:", "Add User", JOptionPane.PLAIN_MESSAGE,
-            null, null, "");
+        String s = "";
         try {
-          Signin.main(new String[0]);
+          s = OAuth2.getEmail();
         } catch (Exception exception) {
           System.out.println(exception);
         }
